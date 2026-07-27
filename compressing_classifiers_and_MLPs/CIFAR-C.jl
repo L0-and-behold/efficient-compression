1d266b73a178a36101a4830acfb0f3bf9c79abff
LUK4S-B <luke.git@posteo.net>
2026-07-27 19:29:46 +0200

    cifar c

--- compressing_classifiers_and_MLPs/CIFAR-C.jl
+++ compressing_classifiers_and_MLPs/CIFAR-C.jl
@@ -45,63 +45,76 @@ begin
     args.dev = Lux.gpu_device()
 end
 
+function total_loss(tstate, dataset, loss_fun)
+    return compute_loss_over_batches(tstate, tstate.parameters, dataset, Float32, loss_fun)
+end
 
 using CSV, DataFrames
 
 model_path = "./src/DatasetsModels/CIFAR-C/tested_models/sweep/1/"
 
-# for entry in readdir(model_path)
-
-tstate_cpu, model, rng = load_train_state("./src/DatasetsModels/CIFAR-C/tested_models/no_shrinking_val/CIFAR_5_val_acc_1-5/artifacts/run-6fa7682b/train_state.bson");
-tstate = to_gpu(tstate_cpu);
-
-tstate_cpu_vanilla, model_vanilla, rng_vanilla = load_train_state("./src/DatasetsModels/CIFAR-C/tested_models/no_shrinking_val/CIFAR_5_val_acc_2-5/artifacts/run-361a3d4e/train_state.bson");
-tstate_vanilla = to_gpu(tstate_cpu_vanilla);
-
-loss_fun = initialize_RL1_loss(tstate, args, logitcrossentropy)
-function total_loss(tstate, dataset)
-    return compute_loss_over_batches(tstate, tstate.parameters, dataset, Float32, loss_fun)
-end
-
 train_set, validation_set, test_set = CIFAR_data(args.train_batch_size, args.dev; seed=1234);
 
-clean_acc = accuracy(tstate,test_set)
-clean_acc_vanilla = accuracy(tstate_vanilla,test_set)
+store = Dict{String, Dict{String, Any}}()
 
-clean_loss = total_loss(tstate, test_set)
-clean_loss_vanilla = total_loss(tstate_vanilla, test_set)
+for entry in readdir(model_path)
+# entry = readdir(model_path)[1]
+    subfolder = joinpath(model_path, entry)
+    isdir(subfolder) || error("Not a directory: $subfolder")
 
-store = Dict{String, Dict{String, Vector{Float32}}}()
+    # extract name, alpha and seed from csv file
+    csv_files = filter(f -> endswith(f, ".csv"), readdir(subfolder))
+    isempty(csv_files) && error("No CSV file found in: $subfolder") # skip if no csv found
+    csvpath = joinpath(subfolder, csv_files[1])
+    df = CSV.read(csvpath, DataFrame)  # first row = header (names), second row = values
+    row = df[1, :] # read the single data row
+    if row.optimization_procedure == "RL1_procedure" && row.α == 0f0 && row.β == 0f0 && row.initial_p_value == 0f0
+        method_name = "vanilla"
+    else
+        method_name = row.optimization_procedure
+    end
+    storage_name = method_name * "_seed-" * string(row.seed) * "_alpha-" * string(row.α)
+    sub_store = get!(store, storage_name, Dict{String, Any}())
+    sub_store["method_name"] = method_name
+    sub_store["seed"] = row.seed
+    sub_store["alpha"] = row.α
+    println(method_name, ", seed = ", row.seed, ", α = ", row.α)
 
-CAs = []
-CAs_vanilla = []
-CEs = []
-CEs_vanilla = []
-for corruption_type in corruption_types
-    println(corruption_type, " = corruption_type")
-    corrupted_test_set = CIFAR_C_data(corruption_type)
+    artifacts_subfolder = subfolder * "/artifacts/"
+    run_subfolder_name = readdir(artifacts_subfolder)[1]
+    run_subfolder = artifacts_subfolder * run_subfolder_name
+    
+    tstate_cpu, model, rng = load_train_state(bson_subfolder * "/train_state.bson");
+    tstate = to_gpu(tstate_cpu);
+    loss_fun = initialize_RL1_loss(tstate, args, logitcrossentropy)
 
-    corrupted_acc = accuracy(tstate, corrupted_test_set)
-    push!(CAs, corrupted_acc)
-    corrupted_acc_vanilla = accuracy(tstate_vanilla, corrupted_test_set)
-    push!(CAs_vanilla, corrupted_acc_vanilla)
-    println(corrupted_acc, " = corrupted_acc")
-    println(corrupted_acc_vanilla, " = corrupted_acc_vanilla")
+    clean_acc = accuracy(tstate,test_set)
+    clean_loss = total_loss(tstate, test_set, loss_fun)
 
-    corrupted_loss = total_loss(tstate, corrupted_test_set)
-    push!(CEs, corrupted_loss)
-    corrupted_loss_vanilla = total_loss(tstate_vanilla, corrupted_test_set)
-    push!(CEs_vanilla, corrupted_loss_vanilla)
-    println(corrupted_loss, " = corrupted_loss")
-    println(corrupted_loss_vanilla, " = corrupted_loss_vanilla")
+    sub_store["clean_acc"] = clean_acc
+    sub_store["clean_loss"] = clean_loss
+
+    CAs = []
+    CEs = []
+    for corruption_type in corruption_types
+        println(corruption_type)
+        corrupted_test_set = CIFAR_C_data(corruption_type)
+
+        corrupted_acc = accuracy(tstate, corrupted_test_set)
+        push!(CAs, corrupted_acc)
+        println(corrupted_acc, " = corrupted_acc")
+
+        corrupted_loss = total_loss(tstate, corrupted_test_set, loss_fun)
+        push!(CEs, corrupted_loss)
+        println(corrupted_loss, " = corrupted_loss")
+        println()
+    end
+
+    sub_store["CAs"] = CAs
+    sub_store["CEs"] = CEs
+
     println()
 end
-
-sub = get!(store, "DRR", Dict{String, Vector{Float32}}())
-sub["CAs"] = CAs
-sub["CAs_vanilla"] = CAs_vanilla
-sub["CEs"] = CEs
-sub["CEs_vanilla"] = CEs_vanilla
 
 open("./src/DatasetsModels/CIFAR-C/tested_models/no_shrinking_val/store.json", "w") do io
     JSON3.pretty(io, store)
